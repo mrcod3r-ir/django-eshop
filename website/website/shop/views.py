c9f89999334781248753ef1e15e9833e47ffa82a
mr cod3r <mrcod3r.ir@gmail.com>
2022-04-15 16:24:07 +0430

    init | p3 completed = template completed

---
+++ website/website/shop/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+  return render(request,'index.html')
+
+def checkout(request):
+  return render(request,'checkout.html')
+
+def product(request):
+  return render(request,'product.html')
+
+def store(request):
+  return render(request,'store.html')
