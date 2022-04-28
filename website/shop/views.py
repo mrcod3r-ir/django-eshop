5b6fd92c7cb821a92ed956903b927e080921c8a7
mr cod3r <mrcod3r.ir@gmail.com>
2022-04-28 12:14:28 +0430

    p5 = 18:00 > show product in home form db

--- website/shop/views.py
+++ website/shop/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-
+from . import models
 # Create your views here.
 
 def index(request):
-  return render(request,'index.html')
+  product_list = models.Product.objects.all()[:5]
+  return render(request,'index.html',{'product_list':product_list})
 
 def checkout(request):
   return render(request,'checkout.html')
